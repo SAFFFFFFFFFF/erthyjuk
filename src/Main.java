d88c4d98672f6f95f84555b6933270f3b6854d02
Tokpoki <4etki_maga@mail.ru>
2024-01-20 07:58:10 +0600

    oop2

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import kz.aitu.dz.Dog;
+
+public class Main {
+    public static void main(String[] args){
+
+        Dog sharik = new Dog("sharik",16,"white");
+        System.out.println(sharik.getName());
+    }
+
+}
